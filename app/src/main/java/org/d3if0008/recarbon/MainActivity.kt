c95ab9efb71234b9720028fe0b2078b2638d576d
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-04-11 18:07:21 +0700

    Membuat View SignIn Layout

--- app/src/main/java/org/d3if0008/recarbon/MainActivity.kt
+++ app/src/main/java/org/d3if0008/recarbon/MainActivity.kt
@@ -1,11 +1,24 @@
 package org.d3if0008.recarbon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnToSignIn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnToSignIn = findViewById(R.id.btn_toSignIn)
+        btnToSignIn.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 }
