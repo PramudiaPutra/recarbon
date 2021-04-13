0b34e4ea4029f40c4cf9f9e8865a14a554f660b3
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-04-13 17:34:43 +0700

    Menambah binding ke SettingsFragment.kt

--- app/src/main/java/org/d3if0008/recarbon/fragment/SettingsFragment.kt
+++ app/src/main/java/org/d3if0008/recarbon/fragment/SettingsFragment.kt
@@ -5,23 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.d3if0008.recarbon.R
+import org.d3if0008.recarbon.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View{
+        binding = FragmentSettingsBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 }
