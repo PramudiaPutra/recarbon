8e680c6a186985337e03c5ab5cf30bea82354acf
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-04-13 20:37:04 +0700

    Menambah tampilan kalkulator (blom ada recycler)

--- app/src/main/java/org/d3if0008/recarbon/fragment/CalculateFragment.kt
+++ app/src/main/java/org/d3if0008/recarbon/fragment/CalculateFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.d3if0008.recarbon.databinding.FragmentCalculateBinding
 
 
@@ -17,7 +18,12 @@ class CalculateFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCalculateBinding.inflate(layoutInflater,container,false)
+        binding = FragmentCalculateBinding.inflate(layoutInflater, container, false)
+
+        BottomSheetBehavior.from(binding.flBottomSheet).apply {
+            peekHeight = 300
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
         return binding.root
     }
 }
