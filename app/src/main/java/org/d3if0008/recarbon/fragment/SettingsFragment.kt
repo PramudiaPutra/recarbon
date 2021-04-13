8acbcfd378cf333a34721738f477fdb42486e611
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-04-13 17:32:03 +0700

    Memindah SettingsFragment.kt ke package fragment

--- app/src/main/java/org/d3if0008/recarbon/fragment/SettingsFragment.kt
+++ app/src/main/java/org/d3if0008/recarbon/fragment/SettingsFragment.kt
@@ -1,10 +1,11 @@
-package org.d3if0008.recarbon
+package org.d3if0008.recarbon.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.d3if0008.recarbon.R
 
 class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
