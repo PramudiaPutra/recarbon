8b2ca85b350821a3ec77480ba4352113c8e89e4f
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-04-17 21:39:20 +0700

    Membuat halaman detail artikel

--- app/src/main/java/org/d3if0008/recarbon/adapter/ArticleAdapter.kt
+++ app/src/main/java/org/d3if0008/recarbon/adapter/ArticleAdapter.kt
@@ -1,43 +1,61 @@
 package org.d3if0008.recarbon.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if0008.recarbon.data.ArticleData
 import org.d3if0008.recarbon.databinding.RecyclerArticleBinding
 
-class ArticleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var items: List<ArticleData> = ArrayList()
+class ArticleAdapter(
+    private val context: Context,
+    private val items: List<ArticleData>,
+    private val listener: (ArticleData) -> Unit
+) : RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
 
-    fun addArticle(article: List<ArticleData>) {
-        items = article
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = RecyclerArticleBinding.inflate(inflater, parent, false)
         return ArticleViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is ArticleViewHolder) {
-            holder.bind(items[position])
-        }
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        holder.bind(items[position], listener)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//        if (holder is ArticleViewHolder) {
+//            holder.bind(items[position], listener)
+//        }
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return items.size
+//    }
+//
+//    fun addArticle(article: List<ArticleData>) {
+//        items = article
+//    }
 
     class ArticleViewHolder(private val binding: RecyclerArticleBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(articleData: ArticleData) = with(binding) {
+        fun bind(
+            articleData: ArticleData, listener: (ArticleData) -> Unit
+        ) = with(binding) {
             titleArticle.text = articleData.title
             contentArticle.text = articleData.content
             profileArticle.text = articleData.author
             ageArticle.text = articleData.age
             imageProfileArticle.setImageResource(articleData.authorImage)
+            itemView.setOnClickListener { listener(articleData) }
         }
     }
 
--- app/src/main/java/org/d3if0008/recarbon/fragment/ArticleFragment.kt
+++ app/src/main/java/org/d3if0008/recarbon/fragment/ArticleFragment.kt
@@ -1,17 +1,22 @@
 package org.d3if0008.recarbon.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import org.d3if0008.recarbon.ArticleActivity
 import org.d3if0008.recarbon.adapter.ArticleAdapter
 import org.d3if0008.recarbon.databinding.FragmentArticleBinding
 import org.d3if0008.recarbon.dummy.ArticleDummy
 
 class ArticleFragment : Fragment() {
+
+    companion object{
+        const val INTENT_ITEM = "OBJECT_INTENT"
+    }
 
     private lateinit var binding: FragmentArticleBinding
     private lateinit var articleAdapter: ArticleAdapter
@@ -24,20 +29,20 @@ class ArticleFragment : Fragment() {
         binding = FragmentArticleBinding.inflate(layoutInflater, container, false)
 
         initRecycler()
-        insertData()
         return binding.root
     }
 
     private fun initRecycler() {
+        val getArticle = ArticleDummy.dataSet()
         binding.rvArticleList.apply {
             layoutManager = LinearLayoutManager(context)
-            articleAdapter = ArticleAdapter()
+            articleAdapter = ArticleAdapter(context,getArticle) {
+                val intent = Intent(context, ArticleActivity::class.java)
+                intent.putExtra(INTENT_ITEM,it)
+                startActivity(intent)
+            }
             adapter = articleAdapter
         }
     }
 
-    private fun insertData() {
-        val data = ArticleDummy.dataSet()
-        articleAdapter.addArticle(data)
-    }
 }
